e8129238f058dcafff04bfc43b4e388c06d0e699
peter592 <petersamy592@gmail.com>
2021-04-23 22:44:12 +0200

    added file input

--- Huffman-compressor/Huffman-compressor/Source.cpp
+++ Huffman-compressor/Huffman-compressor/Source.cpp
@@ -1,1 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
+
+int main() {
+
+
+    FILE *file;
+    char fileName[100];
+    printf("Please enter path to file:\n");
+    scanf("%s", fileName);
+
+    file = fopen(fileName, "r");
+
+    if(!file){
+        printf("file entered can't be found\n");
+        printf("exiting...");
+        return -1;
+    }
+
+    printf("opened");
+
+    fclose(file);
+
+    return 0;
+}
