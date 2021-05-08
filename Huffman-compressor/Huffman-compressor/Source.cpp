3cbaa79f7490e4b00808b88320ef0e62d10f7c5c
Mohamed Essam <mohamedessam12345678@gmail.com>
2021-05-09 01:04:40 +0200

    binaryWritingFile

--- Huffman-compressor/Huffman-compressor/Source.cpp
+++ Huffman-compressor/Huffman-compressor/Source.cpp
@@ -1,24 +1,30 @@
 #include "Utils.h"
 
-int getSizeOfCodesMap(char* codesMap[]){
+int getSizeOfCodesMap(char *codesMap[])
+{
 	int counter = 0;
-	for(int i = 0; i < 256; i++){
-		if(codesMap[i]){
+	for (int i = 0; i < 256; i++)
+	{
+		if (codesMap[i])
+		{
 			counter++;
 		}
 	}
 	return counter;
 }
 
-void serializeCodesMap(char* codesMap[],int sizeOfCodesMap, const char* filePath){
-	 //clearing content "should be opt in future"
+void serializeCodesMap(char *codesMap[], int sizeOfCodesMap, const char *filePath)
+{
+	//clearing content "should be opt in future"
 	//FILE* file1 = fopen(filePath, "w");
 	//fclose(file1);
 
 	FILE *file = fopen(filePath, "a");
-	for(int ch = 0; ch<sizeOfCodesMap; ch++){
-		if(codesMap[ch]){
-			// fwrite(codesMap[i], sizeof(codesMap[i]), sizeOfCodesMap, file);	
+	for (int ch = 0; ch < sizeOfCodesMap; ch++)
+	{
+		if (codesMap[ch])
+		{
+			// fwrite(codesMap[i], sizeof(codesMap[i]), sizeOfCodesMap, file);
 			fprintf(file, "%c%s\n", ch, codesMap[ch]);
 			//printf("%c %s\n", ch, codesMap[ch]);
 		}
@@ -26,14 +32,16 @@ void serializeCodesMap(char* codesMap[],int sizeOfCodesMap, const char* filePath
 	fclose(file);
 }
 // shift all characters one place to the left starting with the given index
-void shiftLeft(char str[]) {
-	for (int i = 0; i < strlen(str); i++) {
+void shiftLeft(char str[])
+{
+	for (int i = 0; i < strlen(str); i++)
+	{
 		str[i] = str[i + 1];
 	}
 }
-void deSerializeCodesMap(char* codesMap[256], const char* filePath){
+void deSerializeCodesMap(char *codesMap[256], const char *filePath)
+{
 	FILE *file = fopen(filePath, "r");
-	
 
 	//char currentChar;
 	//while ((currentChar = fgetc(file)) != EOF) {
@@ -59,56 +67,80 @@ void deSerializeCodesMap(char* codesMap[256], const char* filePath){
 	char line[256];
 	char code[30];
 	initCodesMap(codesMap);
-	while (fgets(line, 45, file)) {
+	while (fgets(line, 45, file))
+	{
 		//concatenateString(code, line);
 		char character = line[0];
 		shiftLeft(line);
 		printf("%c : %s\n", character, line);
-		
+
 		//strcpy(line, codesMap[character] );
-		
+
 		//codesMap[(int)character] = line;
 	}
 	//printf("\n");
 	fclose(file);
-
 }
 
+int binaryWritingFile(char srcName[], char dstName[], char* codesMap[])
+{
+	FILE *src, *dst;
+	src = fopen(srcName, "r");
+	if (!src)
+	{
+		printf("Cannot read input file");
+		return -2;
+	}
+	dst = fopen(dstName, "wb");
+	if (!dst)
+	{
+		printf("Cannot create output file");
+		return -2;
+	}
 
-int main() {
+	char currentChar;
+	while ((currentChar = fgetc(src)) != EOF)
+	{
+		// fprintf(dst, "%s", codesMap[currentChar]);
+		fwrite();
+	}
 
-	Node* LinkedTree = NULL;
+	fclose(src);
+	fclose(dst);
+}
+int main()
+{
+
+	Node *LinkedTree = NULL;
 
 	int frequencyMap[256];
-	char* codesMap[256];
-	char* codesMapNew[256];
+	char *codesMap[256];
+	char *codesMapNew[256];
 	initCodesMap(codesMapNew);
 	char current[50];
 	current[0] = '\0';
-	
-	char path[] = "D:\\stuff\\huffman-compressor\\testingFiles\\3.txt";
+
+	char path[] = "C:\\Users\\Mohamed-Essam\\Desktop\\huffman-compressor\\Huffman-compressor\\Huffman-compressor\\test.txt";
 
 	generateFrequencyTable(path, frequencyMap);
 
 	LinkedTree = generatePriorityQueue(LinkedTree, frequencyMap);
 
-    LinkedTree = buildHuffmanTree(LinkedTree);
+	LinkedTree = buildHuffmanTree(LinkedTree);
 
 	generateCodes(LinkedTree, current, codesMap);
-	
-	
+
 	// copyCodesMapToTable(&allCodesTable, codesMap);
 	// HashTable tabel[getSizeOfCodesMap(codesMap)];
 
 	int codesMapSize = getSizeOfCodesMap(codesMap);
 
 	serializeCodesMap(codesMap, codesMapSize, "D:/stuff/huffman-compressor/testingFiles/codesbin.cod");
+
+	binaryWritingFile(path, "C:\\Users\\Mohamed-Essam\\Desktop\\huffman-compressor\\Huffman-compressor\\Huffman-compressor\\out.com", codesMap);
 	printCodes(codesMap);
 	//deSerializeCodesMap(codesMapNew, "D:/stuff/huffman-compressor/testingFiles/codesbin.cod");
 	//serializeCodesMap(codesMapNew, codesMapSize, "D:/stuff/huffman-compressor/testingFiles/codesbin.cod");
-	
 
-	
-
-    return 0;
+	return 0;
 }
